dcd8e04dcc6088a8ce1573655eceefbc80605500
MhmmdRFadhil <fadhiill2719@gmail.com>
2021-12-04 09:36:14 +0800

    Add profile layout and fix some layout

--- app/src/main/java/com/capstone/urskripsi/ui/content/profile/account/ChangeProfileActivity.kt
+++ app/src/main/java/com/capstone/urskripsi/ui/content/profile/account/ChangeProfileActivity.kt
@@ -2,9 +2,13 @@ package com.capstone.urskripsi.ui.content.profile.account
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.MenuItem
 import com.capstone.urskripsi.R
 import com.capstone.urskripsi.databinding.ActivityChangeProfileBinding
+import com.capstone.urskripsi.utils.Utility.setStateColor
+import com.capstone.urskripsi.utils.Utility.showToast
 import com.capstone.urskripsi.utils.Utility.simpleToolbar
 
 class ChangeProfileActivity : AppCompatActivity() {
@@ -15,6 +19,104 @@ class ChangeProfileActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         simpleToolbar(getString(R.string.change_profile), binding.toolbar.root, true)
+
+        checkInput()
+    }
+
+    private fun checkInput() {
+        binding.apply {
+            edtFullName.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    if (p0.toString().isNotEmpty()) {
+                        toolbar.btnSave.setStateColor(
+                            this@ChangeProfileActivity,
+                            R.color.primaryColor,
+                            true
+                        )
+                    } else {
+                        toolbar.btnSave.setStateColor(
+                            this@ChangeProfileActivity,
+                            R.color.button_disable,
+                            false
+                        )
+                    }
+                }
+
+                override fun afterTextChanged(p0: Editable?) {}
+            })
+
+            edtEmail.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    if (p0.toString().isNotEmpty()) {
+                        toolbar.btnSave.setStateColor(
+                            this@ChangeProfileActivity,
+                            R.color.primaryColor,
+                            true
+                        )
+                    } else {
+                        toolbar.btnSave.setStateColor(
+                            this@ChangeProfileActivity,
+                            R.color.button_disable,
+                            false
+                        )
+                    }
+                }
+
+                override fun afterTextChanged(p0: Editable?) {}
+            })
+
+            edtUniversityName.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    if (p0.toString().isNotEmpty()) {
+                        toolbar.btnSave.setStateColor(
+                            this@ChangeProfileActivity,
+                            R.color.primaryColor,
+                            true
+                        )
+                    } else {
+                        toolbar.btnSave.setStateColor(
+                            this@ChangeProfileActivity,
+                            R.color.button_disable,
+                            false
+                        )
+                    }
+                }
+
+                override fun afterTextChanged(p0: Editable?) {}
+            })
+
+            edtStudyProgram.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    if (p0.toString().isNotEmpty()) {
+                        toolbar.btnSave.setStateColor(
+                            this@ChangeProfileActivity,
+                            R.color.primaryColor,
+                            true
+                        )
+                    } else {
+                        toolbar.btnSave.setStateColor(
+                            this@ChangeProfileActivity,
+                            R.color.button_disable,
+                            false
+                        )
+                    }
+                }
+
+                override fun afterTextChanged(p0: Editable?) {}
+            })
+
+            toolbar.btnSave.setOnClickListener {
+                showToast(getString(R.string.save_success), this@ChangeProfileActivity)
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/com/capstone/urskripsi/utils/Utility.kt
+++ app/src/main/java/com/capstone/urskripsi/utils/Utility.kt
@@ -2,14 +2,20 @@ package com.capstone.urskripsi.utils
 
 import android.content.Context
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 object Utility {
 
-    // custom ActionBar to set Title Color
-    fun AppCompatActivity.simpleToolbar(toolbarTitle: String, toolbar: Toolbar?, navigationIcon: Boolean) {
+    // custom Toolbar
+    fun AppCompatActivity.simpleToolbar(
+        toolbarTitle: String,
+        toolbar: Toolbar?,
+        navigationIcon: Boolean
+    ) {
         setSupportActionBar(toolbar)
         supportActionBar?.apply {
             title = toolbarTitle
@@ -31,5 +37,10 @@ object Utility {
 
     fun Context.getStringFromName(name: String): String {
         return getString(resources.getIdentifier(name, "string", packageName))
+    }
+
+    fun Button.setStateColor(context: Context, backgroundColor: Int, enable: Boolean) {
+        setBackgroundColor(ContextCompat.getColor(context, backgroundColor))
+        isEnabled = enable
     }
 }
