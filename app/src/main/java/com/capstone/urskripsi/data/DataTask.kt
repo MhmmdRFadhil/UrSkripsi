e381c7034643d7ef7ea8ab35dcf73cfe3736cc96
Naufal Abiyyu <naufal1068@gmail.com>
2021-12-17 21:20:31 +0800

    Add DataTask.kt

---
+++ app/src/main/java/com/capstone/urskripsi/data/DataTask.kt
@@ -0,0 +1,13 @@
+package com.capstone.urskripsi.data
+
+object DataTask {
+
+    const val TABLE_NAME = "tasks"
+
+    const val ID = "id"
+
+    const val TITLE = "title"
+
+    const val DESCRIPTION = "description"
+
+}
--- app/src/main/java/com/capstone/urskripsi/data/Task.kt
+++ app/src/main/java/com/capstone/urskripsi/data/Task.kt
@@ -4,19 +4,23 @@ import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.capstone.urskripsi.data.DataTask.DESCRIPTION
+import com.capstone.urskripsi.data.DataTask.ID
+import com.capstone.urskripsi.data.DataTask.TABLE_NAME
+import com.capstone.urskripsi.data.DataTask.TITLE
 
-@Entity(tableName = "tasks")
+@Entity(tableName = TABLE_NAME)
 data class Task(
     @PrimaryKey(autoGenerate = true)
     @NonNull
-    @ColumnInfo(name = "id")
+    @ColumnInfo(name = ID)
     val id: Int = 0,
 
     @NonNull
-    @ColumnInfo(name = "bab")
-    val bab: String,
+    @ColumnInfo(name = TITLE)
+    val title: String,
 
     @NonNull
-    @ColumnInfo(name = "subBab")
-    val subBab: String
+    @ColumnInfo(name = DESCRIPTION)
+    val description: String
 )
